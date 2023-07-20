edb3021a3b6ee4ad3c8b372edab651a13198641e
Pur3-MAlice <aliceford98@live.co.uk>
2023-07-20 20:13:19 +0000

    adds modal for user enter after delete request

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
+
+document.addEventListener('DOMContentLoaded', function() {
+    let modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
+});
