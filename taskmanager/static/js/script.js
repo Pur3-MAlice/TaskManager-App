b04e98f613df303a311d26fdbb394f8ab908191b
Pur3-MAlice <aliceford98@live.co.uk>
2023-07-22 14:34:10 +0000

    replaces fontawesome url and finishes task page

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,9 +1,26 @@
+// sidenav init
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
 
+// modal init
 document.addEventListener('DOMContentLoaded', function() {
     let modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
+});
+
+// datepicker calender init
+document.addEventListener('DOMContentLoaded', function() {
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy", 
+    i18n: {done: "Select"}
+  });
+});
+
+// dropdown init
+document.addEventListener('DOMContentLoaded', function() {
+  let categoryselect = document.querySelectorAll('select');
+  M.FormSelect.init(categoryselect);
 });
